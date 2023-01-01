65f7f496a563a49a40d8de111d51e437f18f7186
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2023-01-01 21:18:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ const secondsHand = document.querySelector("#secondsHand");
 
 function updateTime() {
     let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
 
-    hoursHand.style.transform = `rotate(${((hours / 12) * 360) + ((minutes / 60) * 30) + 90}deg)`;
-    minutesHand.style.transform = `rotate(${((minutes / 60) * 360) + ((seconds / 60) * 6) + 90}deg)`;
-    secondsHand.style.transform = `rotate(${(seconds / 60 * 360) + 90}deg)`;
+    hoursHand.style.transform = `rotate(${((h / 12) * 360) + ((m / 60) * 30) + 90}deg)`;
+    minutesHand.style.transform = `rotate(${((m / 60) * 360) + ((s / 60) * 6) + 90}deg)`;
+    secondsHand.style.transform = `rotate(${(s / 60 * 360) + 90}deg)`;
 
-    digitalClock.innerHTML = `${hours}:${minutes}:${seconds}`
+    digitalClock.innerHTML = `${h}:${m}:${s}`
 }
 
 setInterval(updateTime, 1000);
