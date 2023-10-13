335910d33562bb1ff1c00159dc3ba23b33ee9184
Nandkishor Kumawat <nandkishor41071@gmail.com>
2023-10-13 21:04:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ function updateTime() {
     let h = date.getHours();
     let m = date.getMinutes();
     let s = date.getSeconds();
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
 
     let ha = ((h / 12) * 360) + ((m / 60) * 30) % 360;
     let ma = ((m / 60) * 360) + ((s / 60) * 6) % 360;
@@ -21,7 +18,7 @@ function updateTime() {
     minutesHand.style.rotate = ma + 'deg';
     secondsHand.style.rotate = sa + 'deg';
 
-    digitalClock.innerHTML = `${h}:${m}:${s}`
+    digitalClock.innerHTML = `${String(h).padStart(2, 0)}:${String(m).padStart(2, 0)}:${String(s).padStart(2, 0)}`
 }
 
 setInterval(updateTime, 1000);
